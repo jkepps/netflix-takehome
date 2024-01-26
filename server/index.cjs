7947bd6ace2fc95f8bfe26f4860b573f008cf46b
Jordan Epps <jordan@weconnectrecovery.com>
2024-01-26 09:47:27 -0800

    log errors on the server

--- server/index.cjs
+++ server/index.cjs
@@ -1,22 +1,25 @@
 const express = require('express')
-const app = express()
-const port = 3000
 const cors = require('cors')
 const yelp = require('api')('@yelp-developers/v1.0#8e0h2zlqcimwm0')
 const process = require('process')
 
+const app = express()
+app.use(cors())
+
 yelp.auth(`Bearer ${process.env.YELP_API_KEY}`)
-app.use(cors()).get('/search', async (req, res) => {
+app.get('/yelpSearch', async (req, res) => {
   try {
     const { data, status } = await yelp.v3_business_search(req.query)
     res.status(status)
     res.send(data)
   } catch (error) {
-    res.statusCode = error.status
+    console.error(error)
+    res.status(error.status)
     res.send(error.data)
   }
 })
 
+const port = 3000
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
